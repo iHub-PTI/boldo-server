c0085dd615a0e8abb7d365eb0f63d4b898a64aa4
zommerberg <zommerberg@abv.bg>
2021-08-09 17:36:36 +0300

    add medicine

--- src/server.ts
+++ src/server.ts
@@ -134,11 +134,12 @@ app.get('/profile/doctor', keycloak.protect('realm:doctor'), async (req, res) =>
   try {
     const doctor = await Doctor.findById(req.userId)
     const openHours = doctor?.openHours || { mon: [], tue: [], wed: [], thu: [], fri: [], sat: [], sun: [] }
-
+    console.log(`Bearer ${getAccessToken(req)}`)
     const resp = await axios.get('/profile/doctor', { headers: { Authorization: `Bearer ${getAccessToken(req)}` } })
 
     res.send({ ...resp.data, openHours, new: !doctor })
   } catch (err) {
+    console.log(err)
     handleError(req, res, err)
   }
 })
@@ -192,6 +193,48 @@ app.post('/profile/patient', keycloak.protect('realm:patient'), async (req, res)
   const payload = req.body
   try {
     await axios.put('/profile/patient', payload, { headers: { Authorization: `Bearer ${getAccessToken(req)}` } })
+    res.sendStatus(200)
+  } catch (err) {
+    handleError(req, res, err)
+  }
+})
+
+//
+// MEDICATIONS:
+// Protected routes for managing medications
+// GET /medications - Read medications
+//
+
+app.get('/medications', query('content').isString().optional(), async (req: any, res) => {
+  if (!validate(req, res)) return
+
+  const { content } = req.query as any
+
+  try {
+    const resp = await axios.get(`/medications${content ? `?content=${content}` : ''}`, {
+      headers: { Authorization: `Bearer ${getAccessToken(req)}` },
+    })
+    res.send({ items: resp.data.items })
+  } catch (err) {
+    handleError(req, res, err)
+  }
+})
+
+//
+// ENCOUNTER:
+// Protected routes for managing encounters
+// PUT /profile/doctor/appointments/:id/encounter - Update the encounter
+//
+
+app.put('/profile/doctor/appointments/:id/encounter', keycloak.protect('realm:doctor'), async (req, res) => {
+  if (!validate(req, res)) return
+  const { id } = req.params
+  const { encounterData } = req.body
+
+  try {
+    await axios.put(`/profile/doctor/appointments/${id}/encounter`, encounterData, {
+      headers: { Authorization: `Bearer ${getAccessToken(req)}` },
+    })
     res.sendStatus(200)
   } catch (err) {
     handleError(req, res, err)
@@ -541,6 +584,7 @@ app.get(
       // FIXME: nextAvailability is runing the whole loop again.
       // Could be done in one loop in the case that start = now
       // Also starts two workers. Could start one
+      console.log(availabilities, nextAvailability)
       res.send({ availabilities, nextAvailability })
     } catch (err) {
       handleError(req, res, err)
