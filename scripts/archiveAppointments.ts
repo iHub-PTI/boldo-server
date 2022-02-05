75fa59158707db88165550cf4d1c8cd69cec0e48
Pablo López <pablo.lopez@pti.org.py>
2022-02-05 10:35:51 -0300

    feat: archiveAppointments script now calls core-health endpoint to control encounter status

--- scripts/archiveAppointments.ts
+++ scripts/archiveAppointments.ts
@@ -1,12 +1,37 @@
 import 'dotenv/config'
 
 import mongoose from 'mongoose'
-
+import axios from 'axios'
+import express from 'express'
 import CoreAppointment from '../src/models/CoreAppointment'
 
 // This script updates the status of CoreAppointments Documents in MongoDB 
 // It is executed periodically by a crontab 
 // TODO: make it a ENV var 
+
+var Keycloak = require('keycloak-connect');
+const session = require('express-session');
+axios.defaults.baseURL = process.env.IHUB_ADDRESS!
+export const app = express()
+
+let kcConfig = {
+  realm: 'iHub',
+  'auth-server-url': process.env.KEYCLOAK_ADDRESS,
+  'ssl-required': 'external',
+  resource: 'boldo-doctor',
+  'public-client': true,
+  'verify-token-audience': true,
+  'use-resource-role-mappings': true,
+  'confidential-port': 0
+}
+
+var memoryStore = new session.MemoryStore();
+
+const keycloak = new Keycloak({
+  store: memoryStore
+},
+kcConfig)
+
 
 export const archiveAppointments = async () => {
   await mongoose.connect(`${process.env.MONGODB_URI}`, {
@@ -29,6 +54,14 @@ export const archiveAppointments = async () => {
     console.log(err)
   }
   mongoose.disconnect()
+
+  const authentication =await keycloak.grantManager.obtainDirectly(process.env.BOLDO_ADMIN, process.env.BOLDO_PASS)
+  const boldoToken = authentication.access_token.token
+  const response = await axios.put(`/profile/admin/encounter/status`,{}, {
+    headers: { Authorization: `Bearer ${boldoToken}` },
+  })
+  console.log("Encounter status update: ",response.status)
+  
 }
 
 if (require.main === module) {
